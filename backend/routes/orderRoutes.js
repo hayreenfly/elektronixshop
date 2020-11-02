adbf4e13726ea48a58616c9f0c0596654635a43d
Irene Aguas <hayreenfly@gmail.com>
2020-11-01 20:41:29 -0800

    Created Order Route and Actions for Order Details

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
 
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 
 import { secure } from '../middlewares/authMiddleware.js';
 
 router.route('/').post(secure, addOrderItems);
+router.route('/:id').get(secure, getOrderById);
 
 export default router;
